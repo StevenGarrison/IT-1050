3e9ec0e1f670e2a415e739b3ab47cfedcfb74f26
Dan <bingmantech@gmail.com>
2014-11-15 16:31:11 -0500

    Added Lab 4-Copy

---
+++ Lab4 - Copy/Lab4/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Person
+    {
+       private string _name;
+       private int _age;
+       
+        //I learned what I have here from Lynda.com.  Are these constructors?
+       public int Age
+       {
+           get { return this._age; }
+           set { this._age = value; }
+       }
+       public string Name
+       {
+           get { return this._name; }
+           set { this._name = value; }
+       }
+       //is this necessary?
+       public Person()
+       {
+          
+       }
+       
+       //what I originally had as constructors
+       public Person (string firstParameter, int secondParameter)
+        {
+           _name = firstParameter;
+           _age = secondParameter;
+        }
+
+       //void method "Speak"
+       public void Speak(string phrase)//one string parameter
+       {
+           Console.WriteLine(_name + " says, " + phrase);
+       }
+       //method "AddtwoIntegers
+       public int AddTwoIntegers (int x, int y)
+       {
+          int Sum = x + y;
+          Console.WriteLine(_name + " says the sum of 2+5 is " + (Sum));
+          return Sum;
+       }
+    }
+}
---
+++ Lab4 - Copy/Lab4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Console.WriteLine("Please enter your name:  ");
+            person1.Name = Console.ReadLine();
+            Console.WriteLine("Please enter your age:  ");
+            person1.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello " + person1.Name);
+
+            Console.WriteLine();
+            person1.Speak("Oh what a beautiful morning!");
+
+            Console.WriteLine();
+            person1.AddTwoIntegers(2, 5);
+
+            /* Console.WriteLine("What is your name?  ");
+                string name = Console.ReadLine();
+
+            
+         
+                 Console.WriteLine("What is your age?  ");
+                 int age = int.Parse(Console.ReadLine());
+                 */
+
+
+
+            //     //Console.WriteLine();
+            //     p1.Speak("Oh what a beautiful morning!");
+
+            //     public void AddTwoIntegers(int integer1, int integer2)
+            //     {
+
+            //       int Sum = integer1 + integer2;
+            //     }
+            //     }
+            //}
+        }
+    }
+}
+
